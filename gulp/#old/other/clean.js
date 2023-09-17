2feba88d5f2bbecbd0a5ea2df3a91520e077c8f5
Александр Ильин <aleksandr-ilin@integlab.ru>
2023-09-17 23:29:16 +0500

    remove old gulp files

--- gulp/#old/other/clean.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import del from 'del';
-
-export const clean = () => {
-  return del(app.path.clean);
-};
--- gulp/#old/other/json.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export const json = () => {
-  return app.gulp
-    .src(app.path.src.data)
-    .pipe(app.gulp.dest(app.path.build.data));
-};
